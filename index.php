4884f784ef71cfaeaa59f543344518d6b4dae406
Avallah <fayuthr4@gmail.com>
2024-04-10 09:28:40 -0500

    fayu

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Project</title>
+
+    <link rel="stylesheet" src="./css/styles.css">
+</head>
+
+<body>
+
+
+    
+    <script src="./php/index.php"></script>
+</body>
+
+</html>
